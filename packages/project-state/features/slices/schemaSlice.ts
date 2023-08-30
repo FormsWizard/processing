050daad0fb883da16f5fa75f6a7ccf39a4dd5ca9
Johannes Lötzsch <git@johannesloetzsch.de>
2023-08-30 21:03:27 +0200

    setUiSchema

--- packages/project-state/features/slices/schemaSlice.ts
+++ packages/project-state/features/slices/schemaSlice.ts
@@ -1,10 +1,10 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../state/store";
-import { JsonSchema } from '@jsonforms/core'
+import { JsonSchema, UISchemaElement } from '@jsonforms/core'
 
 export interface SchemaState {
   jsonSchema?: JsonSchema
-  uiSchema?: any
+  uiSchema?: UISchemaElement
 }
 
 const initialState: SchemaState = {
@@ -18,11 +18,17 @@ export const schemaSlice = createSlice({
   reducers: {
     setJsonSchema: (state: SchemaState, action: PayloadAction<JsonSchema>) => {
       state.jsonSchema = action.payload;
+    },
+    setUiSchema: (state: SchemaState, action: PayloadAction<UISchemaElement>) => {
+      state.uiSchema = action.payload;
+    },
+    setSchemaState: (state: SchemaState, action: PayloadAction<SchemaState>) => {
+      state = {...state, ...action.payload};
     }
   }
 });
 
-export const { setJsonSchema } = schemaSlice.actions;
+export const { setJsonSchema, setUiSchema, setSchemaState } = schemaSlice.actions;
 
 export const selectJsonSchema = (state: RootState) => state.schema.jsonSchema || initialState.jsonSchema;
 export const selectUiSchema = (state: RootState) => state.schema.uiSchema || initialState.uiSchema;
